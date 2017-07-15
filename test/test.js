2ee5b0f2f14cb51f5a8ad89730bfb36861fd8186
Ján Bušfy <busfy.jan@gmail.com>
2017-07-15 16:58:04 +0200

    Basic tests added

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-let srcDir = __dirname + '/workspaces/src';
+/*let srcDir = __dirname + '/workspaces/src';
 let destDir = __dirname + '/workspaces/dest';
 let synchronizator = require('../index.js');
 
@@ -13,4 +13,48 @@ synchronizator(
             "dirName/test.txt"
         ]
     }
-);
+);*/
+
+let assert = require('assert');
+
+describe('options\Preparer', () => {
+    let OptionsPreparer = require('../src/options/Preparer.js');
+    let optionsPreparer = new OptionsPreparer();
+    
+    describe('#constructor', () => {
+        it('should return object when instantiated', () => {
+            assert.equal(
+                typeof(optionsPreparer),
+                "object"
+            );
+        });
+    });
+    
+    describe('#prepare', () => {
+        let sourceOptions = [
+            undefined,
+            null,
+            false,
+            0,
+            -1,
+            {}
+        ];
+        
+        it('should always return an object', () => {
+            sourceOptions.forEach((options) => {
+                assert.equal(
+                    typeof(optionsPreparer.prepare(options)),
+                    "object"
+                );
+            });
+        });
+        
+        it('should always return not empty object', () => {
+            sourceOptions.forEach((options) => {
+                assert.ok(
+                    Object.keys(optionsPreparer.prepare(options)).length > 0
+                );
+            });
+        });
+    });
+});
